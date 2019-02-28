aabcd9b6267a8a77e025defc4cf0d6f7a46c0236
oqkr <oqkr@protonmail.com>
2019-02-27 18:42:28 -0600

    🦄 Add Bot class

---
+++ src/Bot.ts
@@ -0,0 +1,23 @@
+import { Client, ClientOptions } from 'discord.js';
+
+import Config from './config';
+
+/** Wraps a Discord#Client to add a Config. */
+class Bot extends Client {
+  constructor(readonly config: Config, options: ClientOptions = {}) {
+    super(options);
+  }
+
+  /**
+   * Logs in using Discord#Client.login.
+   *
+   * @param token A Discord API token. If not supplied, this uses a token from
+   *     this.config if present.
+   * @returns The token used.
+   */
+  async login(token?: string): Promise<string> {
+    return super.login(token || this.config.get('token'));
+  }
+}
+
+export default Bot;
