fceb7589970ed68a5f79121cdf444bdf507419b0
oqkr <oqkr@protonmail.com>
2019-03-05 02:45:38 -0600

    🎨 Remove extraneous non-default export

--- src/errors/Exception.ts
+++ src/errors/Exception.ts
@@ -1,9 +1,7 @@
 /** Base class for all custom errors in this package. */
-export class Exception extends Error {
+export default class Exception extends Error {
   constructor(...args: string[]) {
     super(...args);
     if (Error.captureStackTrace) Error.captureStackTrace(this, Exception);
   }
 }
-
-export default Exception;
