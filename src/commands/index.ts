9be5c765d7e682ca049ecf3e252100bd30e8b0bb
oqkr <oqkr@protonmail.com>
2019-03-05 21:46:26 -0600

    ✨ Add mod command
    This will be the root command for all moderator commands, e.g.,
    `%mod kick some-rascally-user`.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,9 +2,10 @@ import { CommandContainer } from '../command';
 
 import cat from './cat';
 import dog from './dog';
+import mod from './mod'
 import ping from './ping';
 import xkcd from './xkcd';
 
-const commands = new CommandContainer(cat, dog, ping, xkcd);
+const commands = new CommandContainer(cat, dog, mod, ping, xkcd);
 
 export default commands;
---
+++ src/commands/mod/index.ts
@@ -0,0 +1,43 @@
+import { Message } from 'discord.js';
+
+import { Command, CommandContainer, runCommand } from '../../command';
+import log from '../../log';
+import { allowedToUse } from '../../utils';
+
+const commands = new CommandContainer();
+
+const usage = `
+Usage: mod <command>
+       mod help <command>
+
+Invokes one of the registered moderation commands.
+
+Users must have moderator role or higher to access this.
+`;
+
+/** Parent mod command for invoking mod subcommands. */
+const mod: Command = {
+  name: 'mod',
+  usage,
+  allowedBy: 'Moderator',
+
+  async run(message: Message, ...args: string[]): Promise<void> {
+    if (!args.length || (args.length === 1 && args[0] === 'help')) {
+      await message.channel.send(`\`\`\`${usage}\`\`\``);
+      return;
+    }
+    const user = await message.guild.fetchMember(message.author);
+    if (!allowedToUse(user, this)) {
+      await message.reply("fuck outta here dude you can't use this");
+      return;
+    }
+    const [command, ...args2] = args;
+    try {
+      await runCommand(message, commands, command, ...args2);
+    } catch (err) {
+      log.error(`caught exception running ${command}: ${err}`);
+    }
+  },
+};
+
+export default mod;
