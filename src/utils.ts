617006f5771c59f50ccea6d1d0d65ad19311fea7
oqkr <oqkr@protonmail.com>
2019-03-07 02:15:56 -0600

    ✨ Add utility function to parse IDs from mentions

--- src/utils.ts
+++ src/utils.ts
@@ -39,3 +39,29 @@ function isAllowedByRole(user: GuildMember, command: Command): boolean {
   if (!role) return false;
   return user.highestRole.comparePositionTo(role) >= 0;
 }
+
+/**
+ * Return a user ID from input string.
+ * @param mention A user mention or user ID.
+ * @example
+ * // Get user from nickname mention (returns 123456789987654321).
+ * userIDFromString(guild, '<@!123456789987654321>')
+ */
+export function userIDFromString(mention: string): string {
+  return mention.replace(/[<@!>]/g, '');
+}
+
+/**
+ * Return User from input string.
+ * @param guild The guild to search in
+ * @param mention A user mention or user ID.
+ * @example
+ * // Get user by nickname mention.
+ * guildMemberFromString(guild, '<@!123456789987654321>');
+ */
+export async function guildMemberFromString(
+  guild: Guild,
+  mention: string
+): Promise<GuildMember> {
+  return guild.fetchMember(userIDFromString(mention));
+}
