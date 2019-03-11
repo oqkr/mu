854c26cda0302b1098a8646194588b052041abb3
oqkr <oqkr@protonmail.com>
2019-03-10 22:32:11 -0500

    ✏️ Fix typo

--- src/providers.ts
+++ src/providers.ts
@@ -1,4 +1,4 @@
-/** An interface for objects that can carry a converstation with a user. */
+/** An interface for objects that can carry a conversation with a user. */
 export interface ChatProvider {
 
   /** Returns a conversational response for its input. */
